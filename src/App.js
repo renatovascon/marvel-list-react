5738a4e999daa99f074eff832b74f2e4c4aeb3f6
Renato Vasconcelos Coura Soares <renato@Renatos-MacBook-Pro.local>
2024-01-29 17:39:48 -0300

    added Heroes component to show heroe list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Heroes from './components/Heroes';
 
 function App() {
   return (
   <>
   <Header />
   <main>
+    <Heroes/>
   </main>
   <footer>
   </footer></>
