25282fefd34e35bd94621d14fba3fcc550e93e8b
Renato Vasconcelos Coura Soares <renato@Renatos-MacBook-Pro.local>
2024-01-29 17:41:45 -0300

    added header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react"
 
-import logo from '../assets/logo/Group.png'
+import logo from "../assets/logo/Group.png"
 
-import '../css/header.css'
+import "../css/header.css"
 
-const Header = props => {
+const Header = (props) => {
   return (
     <header>
       <div className="logo">
         <img src={logo} alt="Logo Marvel" />
       </div>
+      <h1>Explore O Universo</h1>
+      <p>Mergulhe no domonio deslumbrante de todos os personagensque voce ama</p>
     </header>
   )
 }
